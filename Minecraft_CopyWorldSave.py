de6169e645eb1c0375ea4d294a4a3a90a65c0991
Jordan Tyler <96508953+Voltorik@users.noreply.github.com>
2023-02-19 13:58:18 -0600

    Uploaded base file

---
+++ Minecraft_CopyWorldSave.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+# assign directory locations
+src_dir = "E:\Minecraft_Servers\Fabric_1.19.2_0.14.13\Altis"
+# Use raw string to ensure dir path wiht numbers is read correctly
+dest_dir = r'E:\MultiMC\instances\1.19.3\.minecraft\saves\Altis' 
+
+# if directory exists remove it
+if (os.path.isdir(dest_dir)):
+    print(dest_dir, "deleted")
+    shutil.rmtree(dest_dir)
+
+# copy minecraft world directory to single player world directory
+shutil.copytree(src_dir, dest_dir)
+print("World Copied Sucessfully")
+
